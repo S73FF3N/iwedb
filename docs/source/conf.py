025b702b19973a9f0b9f506654ba4d37a68c8bc5
S73FF3N <S713FF3N@4261811f904c>
2020-03-11 07:55:20 +0000

    2.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Steffen Schwarzwald'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '2.1.2'
 
 
 # -- General configuration ---------------------------------------------------
