1d4e1ec89ffa5d7df72bf1d1e8a5fbda50512ba6
S73FF3N <S713FF3N@4261811f904c>
2020-03-23 11:31:59 +0000

    commit 7 from 23.03.2020

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -86,8 +86,8 @@ html_theme = 'sphinx_rtd_theme'
 #        "relbarbgcolor": "black"
 #        }
 
-#locale_dirs = ['locale/']
-#gettext_compact = False
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
